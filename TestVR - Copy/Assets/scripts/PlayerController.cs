50194f63929c0ef2a25c58bf0423110015177df0
TheFirstSaiyan <19hari98@gmail.com>
2018-06-08 20:59:17 +0530

    first commit

---
+++ TestVR - Copy/Assets/scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+   
+
+    private Vector3 direction;
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+       
+        transform.position += Time.deltaTime * Camera.main.transform.forward * 2.5f;
+        transform.position = new Vector3(transform.position.x, -0.5f, transform.position.z);
+           
+        
+        
+		
+	}
+}
---
+++ TestVR - Copy/Assets/scripts/followPlayer.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class followPlayer : MonoBehaviour {
+
+
+    private NavMeshAgent nav;
+    private GameObject player;
+   public Animator animator;
+    private float t=0;
+
+	void Start () {
+        player = GameObject.Find("player");
+        nav = GetComponent<NavMeshAgent>();
+        nav.speed = Random.Range(1f, 3f);
+
+        nav.acceleration = Random.Range(1f, 10f);
+        
+       
+    }
+	
+	void Update () {
+        t += Time.deltaTime;
+        if(t>2.5f)
+        nav.destination = player.transform.position;
+        if (Vector3.Distance(transform.position, player.transform.position) <= 5f)
+         {
+             animator.SetTrigger("EnemyNear");
+        }
+        else
+            animator.SetTrigger("EnemyAway");
+
+
+
+      
+    }
+}
